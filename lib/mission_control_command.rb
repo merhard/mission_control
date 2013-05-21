b00ff380026bad9ce0cfbfa1cd404796a58de9fa
Dan Pickett <dan.pickett@launchware.com>
2013-05-21 15:41:29 -0400

    writes a check in file

--- lib/mission_control_command.rb
+++ lib/mission_control_command.rb
@@ -3,8 +3,15 @@ class MissionControlCommand
     @cmd = cmd
   end
 
+  def self.check_in_path
+    ENV['HOME'] + '/.check_in'
+  end
+
   def output
     if @cmd == 'in'
+      File.open(self.class.check_in_path, 'w') do |f|
+        f.puts 'checkedin'
+      end
       'You are checked in'
     else
       'You are checked out'
--- spec/mission_control_command_spec.rb
+++ spec/mission_control_command_spec.rb
@@ -3,10 +3,26 @@ require 'rspec'
 require_relative '../lib/mission_control_command'
 
 describe MissionControlCommand do
-  it 'acknowledges my check in' do
-    cmd = MissionControlCommand.new('in')
-    expect(cmd.output).to include('You are checked in')
+  it 'has a check in path'
+
+  context 'checking in' do
+    it 'acknowledges my check in' do
+      cmd = MissionControlCommand.new('in')
+      expect(cmd.output).to include('You are checked in')
+    end
+
+    it 'creates a check in file' do
+      cmd = MissionControlCommand.new('in')
+      check_in_path = cmd.class.check_in_path
+      if FileTest.exists?(check_in_path)
+        FileUtils.rm(check_in_path)
+      end
+
+      cmd.output
+      expect(check_in_path).to be_true
+    end
   end
+
 
   it 'acknowledges my check out' do
     cmd = MissionControlCommand.new('out')
