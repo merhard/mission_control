f6ec41df1b5f2b01ad9cd73f51f4c5f82c3ddaf6
jhylau <jhylau1@gmail.com>
2013-05-21 19:20:47 -0700

    Working Prototype

--- lib/mission_control_command.rb
+++ lib/mission_control_command.rb
@@ -1,4 +1,8 @@
+require 'time'
+require 'fileutils'
+
 class MissionControlCommand
+  
   def initialize(cmd)
     @cmd = cmd
   end
@@ -9,12 +13,38 @@ class MissionControlCommand
 
   def output
     if @cmd == 'in'
-      File.open(self.class.check_in_path, 'w') do |f|
-        f.puts 'checkedin'
-      end
-      'You are checked in'
+      check_in
+    elsif @cmd == 'out'
+      check_out
     else
-      'You are checked out'
+      'Please call a valid command!'
+    end
+  end
+
+
+  def check_in
+    if FileTest.exists?(self.class.check_in_path)
+      'You have already checked in!'
+    else
+      File.open(self.class.check_in_path, 'w') do |f|
+        f << Time.now.to_s
+      end
+     "You are checked in as of: #{Time.now.hour}:#{Time.now.strftime('%m')}"
+   end
+ end
+
+  def check_out
+    if FileTest.exists?(self.class.check_in_path)
+      string = ''
+      File.open(self.class.check_in_path).each_line do |line|
+          string = line
+      end
+      difference = Time.now - Time.parse(string)
+      minutes = (difference / 60).round(0)
+      FileUtils.rm(self.class.check_in_path)
+      "You are checked out. Total time spent is: #{minutes} minutes"
+    else
+      'You have not checked in yet!'
     end
   end
 end
--- spec/mission_control_command_spec.rb
+++ spec/mission_control_command_spec.rb
@@ -1,33 +1,97 @@
 require 'rspec'
+require 'time'
 
 require_relative '../lib/mission_control_command'
 
 describe MissionControlCommand do
-  it 'has a check in path'
+ 
 
   context 'checking in' do
+    let(:cmd) {cmd = MissionControlCommand.new('in')}
+    let(:check_in_path) {cmd.class.check_in_path}
+
     it 'acknowledges my check in' do
-      cmd = MissionControlCommand.new('in')
+      if FileTest.exists?(check_in_path)
+        FileUtils.rm(check_in_path)
+      end
       expect(cmd.output).to include('You are checked in')
     end
 
     it 'creates a check in file' do
-      cmd = MissionControlCommand.new('in')
-      check_in_path = cmd.class.check_in_path
       if FileTest.exists?(check_in_path)
         FileUtils.rm(check_in_path)
       end
 
       cmd.output
-      expect(check_in_path).to be_true
+      expect(FileTest.exists?(check_in_path)).to be_true
+    end
+
+    it 'checks if file exists before creating' do
+      File.open(check_in_path, 'w') do |f|
+          f << ' '
+      end
+      expect(cmd.output).to eq('You have already checked in!')
+    end
+
+    it 'puts the time into the file' do
+      if FileTest.exists?(check_in_path)
+        FileUtils.rm(check_in_path)
+      end
+      cmd.output
+      string = ''
+      File.open(check_in_path).each_line do |line|
+        string = line
+      end
+      expect(Time.parse(string).to_s).to include(Time.now.to_s)
+    end
+
+    it 'returns the check in time' do
+      if FileTest.exists?(check_in_path)
+        FileUtils.rm(check_in_path)
+      end
+      expect(cmd.output).to include("#{Time.now.hour}:#{Time.now.min}")
+    end
+
+  end
+
+
+  context 'checking out' do
+    let(:cmd) {cmd = MissionControlCommand.new('out')}
+    let(:check_in_path) {cmd.class.check_in_path}
+
+    before(:each) do
+      unless FileTest.exists?(check_in_path)
+        File.open(check_in_path, 'w') do |f|
+          f << "#{Time.now}"
+        end
+      end
+    end
+
+    it 'acknowledges my check out' do
+      expect(cmd.output).to include('You are checked out')
+    end
+
+    it 'removes a check in file' do
+      cmd.output
+      expect(FileTest.exists?(check_in_path)).to be_false
+    end
+
+    it 'returns you have not checked in yet so you cannot check out' do
+      if FileTest.exists?(check_in_path)
+        FileUtils.rm(check_in_path)
+      end
+      expect(cmd.output).to eql("You have not checked in yet!")
+    end
+
+    it 'returns your time difference' do
+      string = ''
+      File.open(check_in_path).each_line do |line|
+          string = line
+      end
+      difference = Time.now - Time.parse(string)
+      minutes = (difference / 60).round(0)
+      cmd = MissionControlCommand.new('out')
+      expect(cmd.output).to include(minutes.to_s)
     end
   end
-
-
-  it 'acknowledges my check out' do
-    cmd = MissionControlCommand.new('out')
-    expect(cmd.output).to include('You are checked out')
-  end
-
-
 end
